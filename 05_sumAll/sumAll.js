da11521eb42df945869b22849ff0a455eb9967d9
zeeny <157370421+kzeen@users.noreply.github.com>
2024-03-04 21:02:29 +0200

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    if (typeof int1 !== "number" || typeof int2 !== "number" || int1 < 0 || int2 < 0) {
+        return "ERROR";
+    }
+    let total = 0;
+    // Start from smallest number up to the biggest, no matter the order of arguments
+    for (let i=((int1 > int2) ? int2 : int1);i<=((int1 > int2) ? int1 : int2);i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
