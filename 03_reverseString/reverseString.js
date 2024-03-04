090eb43ac3681dc053afff9a1683bb94e34a255f
zeeny <157370421+kzeen@users.noreply.github.com>
2024-03-04 14:53:05 +0200

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reversed = "";
+    for (let i=0;i<str.length;i++) {
+        reversed = str[i] + reversed;
+    }
+    return reversed;
 };
 
 // Do not edit below this line
