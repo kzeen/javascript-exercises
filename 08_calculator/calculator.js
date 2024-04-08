b6c16ede46a7fe91da27ac650daa61ffe68f4263
zeeny <157370421+kzeen@users.noreply.github.com>
2024-04-08 19:52:14 +0300

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce(((total, curr) => total + curr), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce(((total, curr) => total * curr), 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  // Use spread operator to create an array from 0 to num 
+  // slice to not include 0
+  // Multiply all numbers in the array, default is 1
+	return [...Array(num+1).keys()].slice(1).reduce(((total, curr) => total * curr), 1);
 };
 
 // Do not edit below this line
