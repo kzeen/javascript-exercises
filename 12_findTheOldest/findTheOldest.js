4e65bfda11f04c0cf9ae0dbbff35885b080d6dd3
zeeny <157370421+kzeen@users.noreply.github.com>
2024-04-09 16:42:32 +0300

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arrayOfPeopleObjects) {
+    return arrayOfPeopleObjects.sort(function(a, b) {
+        if (!("yearOfDeath" in b)) {
+            b.yearOfDeath = 2024;
+        }
+        if (!("yearOfDeath" in a)) {
+            b.yearOfDeath = 2024;
+        }
+        return (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1 : 1;
+    })[0];
 };
 
 // Do not edit below this line
