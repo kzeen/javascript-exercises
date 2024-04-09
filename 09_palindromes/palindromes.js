9a6b5b74e56720a7829ced54651b2ecd1428d61f
zeeny <157370421+kzeen@users.noreply.github.com>
2024-04-09 12:03:20 +0300

    Complete 09_palindromes
    Used programming logic instead of pure javascript methods

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+ const sanitizedStr = str.toLowerCase().replace(/[.,!?;\s]/g, "");
+ 
+ let left = 0;
+ let right = sanitizedStr.length - 1;
+ 
+ while (left <= right) {
+    if (!(sanitizedStr.charAt(left) === sanitizedStr.charAt(right))) {
+        return false;
+    }
+    left++;
+    right--;
+ }
+ return true;
 };
 
 // Do not edit below this line
